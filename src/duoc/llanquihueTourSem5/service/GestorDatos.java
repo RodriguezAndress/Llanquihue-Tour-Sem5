f3dcc2f543d6558ebde17a621155a1bd1fadfada
Andres <111201799+RodriguezAndress@users.noreply.github.com>
2026-06-28 23:08:24 -0400

    Semana 6, formativa

--- src/duoc/llanquihueTourSem5/service/GestorDatos.java
+++ src/duoc/llanquihueTourSem5/service/GestorDatos.java
@@ -1,24 +1,47 @@
 package duoc.llanquihueTourSem5.service;
 
-import duoc.llanquihueTourSem5.model.Tour;
+import duoc.llanquihueTourSem5.model.ExcursionCultural;
+import duoc.llanquihueTourSem5.model.PaseoLacustre;
+import duoc.llanquihueTourSem5.model.RutaGastronomica;
+import duoc.llanquihueTourSem5.model.ServicioTuristico;
 import duoc.llanquihueTourSem5.util.Archivo;
 import java.util.ArrayList;
 
 public class GestorDatos {
-    private ArrayList<Tour> tours;
+    public static ArrayList<ServicioTuristico> crearTours(){
 
-    public GestorDatos() {
-        //Se crea una lista vacia
-        tours = new ArrayList<>();
+        ArrayList<ServicioTuristico> tours = new ArrayList<>();
+
+        //Agg obj del padre
+        tours.add(new ServicioTuristico("Recorrido Centro Historico", 1.45));
+        tours.add(new ServicioTuristico("Buceo con Tiburones", 3.30));
+
+        //Agg obj de hijos
+        tours.add(new RutaGastronomica("Recorrido Paseo Montalban", 2.45, 5));
+        tours.add(new RutaGastronomica("Camino Historico Precolonial", 2.30, 6));
+
+        //agg obj hijo paseoLacustre
+        tours.add(new PaseoLacustre("Lago Negro", 1.45, "Canoa"));
+        tours.add(new PaseoLacustre("Laguna Piedra Azul", 2.15, "Canoa"));
+
+        //Agg obj hijo ExcursionCultural
+        tours.add(new ExcursionCultural("Cerro San Cristobal", 3.15, "Santiago"));
+        tours.add(new ExcursionCultural("Teatro Municipal de Santiago", 1.45, "Teatro"));
+
+        //guardar en el archivo
+        Archivo.guardarLista(tours);
+
+        //leer desde el archivo
+        return Archivo.leerLista();
+
+
     }
-
-    public void cargarTours() {
-        tours = Archivo.leerLista();
-    }
-
-    public void mostrarTours() {
-        for (Tour t : tours) {
-            System.out.println(t.getNombre() + " ; " + t.getTipo() + " ; $" + t.getPrecio());
+    public static void mostrarTours(ArrayList<ServicioTuristico> tours) {
+        for (ServicioTuristico t : tours) {
+            System.out.println(t.mostrar());
+            //System.out.println("Tour: " + t.getNombre() + " - " + "Duración: " + t.getDuracionHoras() + " hrs");
         }
     }
+
+
 }
--- src/duoc/llanquihueTourSem5/util/Archivo.java
+++ src/duoc/llanquihueTourSem5/util/Archivo.java
@@ -1,21 +1,21 @@
 package duoc.llanquihueTourSem5.util;
 
 import java.io.*;
-import duoc.llanquihueTourSem5.model.Tour;
+import duoc.llanquihueTourSem5.model.ServicioTuristico;
 import java.util.ArrayList;
 
 public class Archivo {
     private static final String DIRECTORIO = "src/resources";
     private static final String ARCHIVO = DIRECTORIO + "/recorridos.txt";
 
-    public static void guardarLista(ArrayList<Tour> lista) {
+    public static void guardarLista(ArrayList<ServicioTuristico> lista) {
         File dir = new File(DIRECTORIO);
         if (!dir.exists()) {
             dir.mkdirs();
         }
 
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(ARCHIVO))) {
-            for (Tour t : lista) {
+            for (ServicioTuristico t : lista) {
                 bw.write(t.toString()); // usa el toString del Tour
                 bw.newLine();
             }
@@ -24,12 +24,12 @@ public class Archivo {
         }
     }
 
-    public static ArrayList<Tour> leerLista() {
-        ArrayList<Tour> lista = new ArrayList<>();
+    public static ArrayList<ServicioTuristico> leerLista() {
+        ArrayList<ServicioTuristico> lista = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(ARCHIVO))) {
             String linea;
             while ((linea = br.readLine()) != null) {
-                lista.add(Tour.fromString(linea)); // reconstruye el objeto
+                lista.add(ServicioTuristico.fromString(linea)); // reconstruye el objeto
             }
         } catch (IOException e) {
             System.out.println("Fallo al leer el archivo: " + e.getMessage());
